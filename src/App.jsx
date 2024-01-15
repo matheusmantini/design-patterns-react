20aa6541fa020bab2ce8f87c44d9c53354ce0ae6
matheusmantini <engmatheusmantini@gmail.com>
2024-01-15 11:30:58 -0300

    Add uncontrolled flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
 import "./App.css";
-import { ControlledModal } from "./components/controlled-modal";
+import { UncontrolledFlow } from "./components/uncontrolled-flow";
 
-function App() {
-  const [shouldDisplay, setShouldDisplay] = useState(false);
-
+const StepOne = ({ goNext }) => {
   return (
     <>
-      <h1>Start Here!</h1>
-      <ControlledModal
-        shouldDisplay={shouldDisplay}
-        onClose={() => setShouldDisplay(false)}
+      <h1>Step #1: Enter your name</h1>
+      <button onClick={() => goNext({ name: "MyName" })}>Next</button>
+    </>
+  );
+};
+
+const StepTwo = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #2: Enter your age</h1>
+      <button onClick={() => goNext({ age: 23 })}>Next</button>
+    </>
+  );
+};
+
+const StepThree = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #3: Enter your Country</h1>
+      <button onClick={() => goNext({ country: "MyCountry" })}>Next</button>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <UncontrolledFlow
+        onDone={(data) => {
+          console.log(data);
+          alert("Yaeem you made it to the final step!");
+        }}
       >
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quis iste molestiae soluta distinctio obcaecati minima accusamus laudantium voluptatibus qui quae magni ipsa, saepe aut dicta suscipit blanditiis beatae itaque?</p>
-      </ControlledModal>
-      <button onClick={() => setShouldDisplay(!shouldDisplay)}>
-        {shouldDisplay ? "Hide Modal" : "Display Modal"}
-      </button>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledFlow>
     </>
   );
 }
