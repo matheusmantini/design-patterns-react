5886b98710ef24a381a6210f231880144c0706f0
matheusmantini <engmatheusmantini@gmail.com>
2024-01-15 09:08:46 -0300

    Add controlled modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import { ControlledModal } from "./components/controlled-modal";
 
 function App() {
+  const [shouldDisplay, setShouldDisplay] = useState(false);
+
   return (
     <>
       <h1>Start Here!</h1>
+      <ControlledModal
+        shouldDisplay={shouldDisplay}
+        onClose={() => setShouldDisplay(false)}
+      >
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quis iste molestiae soluta distinctio obcaecati minima accusamus laudantium voluptatibus qui quae magni ipsa, saepe aut dicta suscipit blanditiis beatae itaque?</p>
+      </ControlledModal>
+      <button onClick={() => setShouldDisplay(!shouldDisplay)}>
+        {shouldDisplay ? "Hide Modal" : "Display Modal"}
+      </button>
     </>
   );
 }
