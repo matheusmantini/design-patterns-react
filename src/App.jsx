b5662f435c333c283b400f4c086d0cee585cf84f
matheusmantini <engmatheusmantini@gmail.com>
2024-01-19 18:55:14 -0300

    Composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { GreenSmallButton, RedButton } from "./components/composition";
 
 function App() {
   return (
     <>
-      <h1>Start Here!</h1>
+      <RedButton text={"I'm red!"} />
+      <GreenSmallButton text={"I'm a small green button!"} />
     </>
   );
 }
